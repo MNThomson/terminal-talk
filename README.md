ccb75d011afb1b9414aa690fc8d4526bffeb538f
Max Thomson <git@mnt.dev>
2024-05-09 12:33:03 -0700

    BATMAN! (since he didn't have a parent (commit))

---
+++ README.md
@@ -0,0 +1,27 @@
+# Terminal Talk
+
+[<img alt="github" src="https://img.shields.io/badge/github-MNThomson/terminal-talk-bc3f48?style=for-the-badge&labelColor=555555&logo=github" height="20">](https://github.com/MNThomson/terminal-talk)
+[<img alt="crates.io" src="https://img.shields.io/crates/v/terminal-talk.svg?style=for-the-badge&color=fc8d62&logo=rust" height="20">](https://crates.io/crates/terminal-talk)
+
+## What the hell is this???
+
+My primary way of communicating with friends in class is just by typing in my terminal and them looking over my shoulder.
+I kept having to spam <kbd>Ctrl +</kbd> to zoom in so they could see, so this just automatically zooms in, provides a repl-like environment, and then resets the zoom afterwards.
+
+Is it useless? Yes! \
+Do I care? No!
+
+```
+$ cargo install terminal-talk
+
+$ ttalk
+> Hey there, look it's zoomed in and my friend can read it!
+```
+
+#### License
+<sup>
+
+Licensed under `ISC`
+
+</sup>
+
---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use std::{
+    env,
+    io::{self, Write},
+    process,
+};
+
+use ctrlc::set_handler;
+use enigo::{
+    Direction::{Click, Press, Release},
+    Enigo, Key, Keyboard, Settings,
+};
+
+enum Zoom {
+    In(usize),
+    #[allow(dead_code)]
+    Out(usize),
+    Reset,
+}
+
+impl Zoom {
+    fn zoom(self) {
+        let mut enigo = Enigo::new(&Settings::default()).unwrap();
+
+        enigo.key(Key::Control, Press).unwrap();
+        match self {
+            Zoom::In(x) | Zoom::Out(x) => {
+                for _ in 0..x {
+                    enigo
+                        .key(
+                            Key::Unicode(if matches!(self, Zoom::In(_)) {
+                                '+'
+                            } else {
+                                '-'
+                            }),
+                            Click,
+                        )
+                        .unwrap();
+                }
+            }
+            Zoom::Reset => enigo.key(Key::Unicode('0'), Click).unwrap(),
+        }
+        enigo.key(Key::Control, Release).unwrap();
+    }
+}
+
+fn main() {
+    set_handler(move || {
+        Zoom::Reset.zoom();
+        process::exit(0);
+    })
+    .unwrap();
+
+    let zoom_level: usize = env::args()
+        .nth(1)
+        .and_then(|s| s.parse::<usize>().ok())
+        .unwrap_or(25);
+
+    Zoom::In(zoom_level).zoom();
+
+    let input = &mut String::new();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let _ = io::stdin().read_line(input);
+    }
+}
